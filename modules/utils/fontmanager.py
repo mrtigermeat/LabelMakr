2692c6d96f3eca059bc3bc2004fb278d692e6043
mrtigermeat <tyler@tigermeat.xyz>
2026-04-04 21:55:54 -0500

    more work on the font manager

--- modules/utils/fontmanager.py
+++ modules/utils/fontmanager.py
@@ -21,18 +21,21 @@ class FontManager:
 
         self.fonts = {
             # 'lang_code_first_2': ('Font name', size, Path(ASSETS / 'fontname.tff/otf'))
-            'en': ('Pixel Operator', 16, Path(ASSETS / 'PixelOperator.ttf')),
-            'jp': ('PixelMPlus10', 16, Path(ASSETS / 'PixelMplus10-Regular.tff')),
-            'ko': ('NeoDunggeunmo', 16, Path(ASSETS / 'neodgm.tff')),
-            'zh': ('WenQuanYi Bitmap Song 16px', 18, Path(ASSETS / 'WenQuanYi.Bitmap.Song.16px.ttf'))
+            'en': ['Pixel Operator', 16, Path(ASSETS / 'PixelOperator.ttf')],
+            'jp': ['PixelMPlus10', 16, Path(ASSETS / 'PixelMplus10-Regular.tff')],
+            'ko': ['NeoDunggeunmo', 16, Path(ASSETS / 'neodgm.tff')],
+            'zh': ['WenQuanYi Bitmap Song 16px', 18, Path(ASSETS / 'WenQuanYi.Bitmap.Song.16px.ttf')]
         }
         self.avail_fonts = ['en', 'jp', 'ko', 'zh']
     
     def load_temp_font(self, font_path) -> None:
         if font_path.exists():
-            shutil.copy(font_path, TEMPDIR)
+            logger.debug('FONT EXISTS')
+            font_name = os.path.split(str(font_path))[-1]
+            temp_font_path = TEMPDIR / font_name
+            shutil.copy(font_path, temp_font_path)
         subprocess.run(
-            ['fc-cache', '-f', TEMPDIR],
+            ['fc-cache', '-f', temp_font_path],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL
         )
@@ -40,20 +43,25 @@ class FontManager:
 
     def load_font(self, lang: str = 'en_US') -> None:
         OS = get_os()
-        pattern = re.compile(r'[_].*')
-        lang = re.sub(pattern, lang, '')
+        lang = lang[:2]
+        assert len(lang) == 2, logger.error(f'Issue with langcode: {lang}')
         # using 'in list' in case there are multiple langs with the same script (zh_ZH, zh_YUE)
         try:
             if lang in self.avail_fonts:
                 if OS == 'win32':
-                    pyglet.font.add_file(str(lang[2]))
+                    pyglet.font.add_file(self.fonts[lang][2])
                 elif OS == 'linux':
-                    self.load_temp_font(str(lang[2]))
-                return (ctk.CTkFont(family=self.fonts[lang][0], size=self.fonts[lang][1]),
-                        ctk.CTkFont(family=self.fonts[lang][0], size=self.fonts[lang][1]-2))
+                    self.load_temp_font(self.fonts[lang][2])
+                font = ctk.CTkFont(family=self.fonts[lang][0], size=self.fonts[lang][1])
+                font_sm = ctk.CTkFont(family=self.fonts[lang][0], size=self.fonts[lang][1]-2)
+                logger.debug(f"{font.cget('family')}")
+                return font, font_sm
             else:
-                return (ctk.CTkFont(family='monospace', size=12),
-                        ctk.CTkFont(family='monospace', size=10))
+                font = ctk.CTkFont(family='monospace', size=12)
+                font_sm = ctk.CTkFont(family='monospace', size=10)
+                return font, font_sm
         except Exception as e:
             logger.error(f'Unable to load font for lang {lang}:\n\n {e} \n\n')
 
+if __name__ == '__main__':
+    print(1)
